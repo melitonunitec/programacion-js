bdbc86a30e21d80131fc89149093dc7db91fc8fd
melitonunitec <m_eli03@hotmail.com>
2021-08-28 18:41:51 -0500

    este es lo básico
    vamos una cuarte de la programación basica

---
+++ seccion-3/app.js
@@ -0,0 +1,20 @@
+///console.log('hola mundo');
+//convertidor de c a f
+let c;
+let f=90;
+
+//c=(f-32)*(5/9); primera forma
+c=f-32;
+
+c=c*(5/9);
+
+console.log(c);
+
+/*conversion de grados kelvin
+ 
+let k;
+let c=50;
+
+k=c+273.15;
+
+console.log(k +' grados Kelvin');*/
