1bd0a91d48c637c870348dbbad3ac09d2f779dc1
melitonunitec <m_eli03@hotmail.com>
2021-09-08 21:59:25 -0500

    ARRAY
    YA BIENE LO CHIDO OSEA LOS ARREGLOS CON SU LOGICA

---
+++ programacion 101/seccion-5.3 - array/app.js
@@ -0,0 +1,14 @@
+function describirPersona(p){
+console.log('sergio tiene una edad 53 y miden 1.63');
+    
+}
+
+
+let persona={
+nombre : 'milton',
+edad: 32,
+estatura: 1.665
+}
+
+describirPersona(persona);
+
